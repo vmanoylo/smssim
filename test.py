93fec70af3a4464fa6b70265af2198376eca3944
vlad <vladislav.manoylo@gmail.com>
2024-07-21 19:06:50 -0400

    unit test file

---
+++ test.py
@@ -0,0 +1,16 @@
+import unittest
+
+class TestProducer(unittest.TestCase):
+    pass
+
+class TestSender(unittest.TestCase):
+    pass
+
+class TestProgressMonitor(unittest.TestCase):
+    pass
+
+class TestSimulator(unittest.TestCase):
+    pass
+
+if __name__ == '__main__':
+    unittest.main()
