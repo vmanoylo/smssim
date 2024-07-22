cc4cd2d4ab18e13c71de4943626520d888b8db4b
vlad <vladislav.manoylo@gmail.com>
2024-07-21 20:27:33 -0400

    Sender tests

--- test.py
+++ test.py
@@ -19,7 +19,37 @@ class TestProducer(unittest.TestCase):
 
 
 class TestSender(unittest.TestCase):
-    pass
+    def test_passing(self):
+        producer = main.producer(100)
+        passed = 0
+        failed = 0
+
+        def update(success, time):
+            nonlocal passed, failed
+            if success:
+                passed += 1
+            else:
+                failed += 1
+
+        main.sender(producer, update, 0, 0)
+        self.assertEqual(passed, 100)
+        self.assertEqual(failed, 0)
+
+    def test_failing(self):
+        producer = main.producer(100)
+        passed = 0
+        failed = 0
+
+        def update(success, time):
+            nonlocal passed, failed
+            if success:
+                passed += 1
+            else:
+                failed += 1
+
+        main.sender(producer, update, 0, 1)
+        self.assertEqual(passed, 0)
+        self.assertEqual(failed, 100)
 
 
 class TestProgressMonitor(unittest.TestCase):
